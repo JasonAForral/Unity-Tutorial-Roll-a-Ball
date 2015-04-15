49216174484c913ecdc998fdea008c07a60fa381
Tsinoyboi <ajsnoface@gmail.com>
2015-04-15 00:41:08 -0700

    Finished Tutorial
    added score and win condition
    minus web publish

---
+++ Assets/PickupGameInfo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupGameInfo{
+
+    public int scoreValue = 10;
+    //public int ScoreValue { get; set; }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -7,6 +8,19 @@ public class PlayerController : MonoBehaviour {
     public Transform cameraPan;
     public float moveSpeed;
     public float jumpSpeed;
+
+    public Text scoreText;
+    public Text winText;
+
+    int scoreCount;
+    int remainingPickUps;
+
+    void Start()
+    {
+        scoreCount = 0;
+        winText.text = "";
+        UpdateScoreText();
+    }
 
 	void FixedUpdate () {
         Vector3 moveAxis = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
@@ -23,6 +37,21 @@ public class PlayerController : MonoBehaviour {
         if ("Pick Up" == other.gameObject.tag)
         {
             other.gameObject.SetActive(false);
+            //Destroy(other.gameObject.GetComponent<Collider>());
+            //Destroy(other.gameObject,1f);
+            scoreCount++;
+            UpdateScoreText();
+            
+        }
+    }
+
+    void UpdateScoreText()
+    {
+        remainingPickUps = GameObject.FindGameObjectsWithTag("Pick Up").Length;
+        scoreText.text = "Score: " + scoreCount;
+        if (0 >= remainingPickUps)
+        {
+            winText.text = "You Win!";
         }
     }
 }
