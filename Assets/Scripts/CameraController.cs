3cdd9d1ba95361a5011da14aa4bf17ac10b20c03
Tsinoyboi <ajsnoface@gmail.com>
2015-04-14 23:02:01 -0700

    Added camera control and transform direction
    Adds axis force based on camera rotation around Y axis.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform cameraTilt;
+    public Transform cameraZoom;
+
+    public Transform playerTransform;
+    
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position;
+	}
+
+    void Update() {
+        if (Input.GetMouseButton(2))
+        {
+            transform.Rotate(Vector3.up * Input.GetAxis("Mouse X"));
+            cameraTilt.Rotate(Vector3.left * Input.GetAxis("Mouse Y"));
+        }
+    }
+
+    // Update is called once per frame
+	void LateUpdate () {
+        transform.position = playerTransform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    public Transform cameraPan;
+    public float moveSpeed;
+
+	void FixedUpdate () {
+        Vector3 moveAxis = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        GetComponent<Rigidbody>().AddForce(cameraPan.TransformDirection(moveAxis * moveSpeed * Time.deltaTime));
+        
+	}
+}
