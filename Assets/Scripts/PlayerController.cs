c3ea24547a9f297e4c1d2eed267c5fe3fa799203
Tsinoyboi <ajsnoface@gmail.com>
2015-04-14 23:32:59 -0700

    Added tags and deactivate pick ups
    When player collides with pickups, they disappear

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,23 @@ public class PlayerController : MonoBehaviour {
 
     public Transform cameraPan;
     public float moveSpeed;
+    public float jumpSpeed;
 
 	void FixedUpdate () {
         Vector3 moveAxis = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         GetComponent<Rigidbody>().AddForce(cameraPan.TransformDirection(moveAxis * moveSpeed * Time.deltaTime));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody>().AddForce(cameraPan.TransformDirection(Vector3.up * jumpSpeed), ForceMode.Impulse);
+        }
         
 	}
+    void OnTriggerEnter(Collider other)
+    {
+        if ("Pick Up" == other.gameObject.tag)
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+    public Space camera;
+	void Update () {
+        transform.Rotate(new Vector3(15f, 30f, 45f) * Time.deltaTime);
+	}
+}
